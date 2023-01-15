253b8f070db87f76ab842d922525469e258fd5c6
juliocesar014 <juliocesar014@live.com>
2023-01-15 09:14:10 -0300

    finish

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ const puppeteer = require("puppeteer");
   console.log("Entrei na página desejada!");
 
   headings = await page.evaluate(() => {
-    headings_elements = document.querySelectorAll("h4 .title");
+    headings_elements = document.querySelectorAll(".caption");
     headings_array = Array.from(headings_elements);
-    return headings_array.map((heading) => heading.innerText);
+    return headings_array.flatMap((heading) => {
+      const regex = new RegExp("Lenovo", "g");
+      if (regex.test(heading.innerText)) {
+        let text = heading.textContent.replace("\t", "");
+        let textSplit = text.split("\n");
+        let dataObj = {};
+        dataObj["price"] = textSplit[1].replace("\t\t", "");
+        dataObj["title"] = textSplit[3].replace("\t\t\t\t\t\t\t\t\t", "");
+        dataObj["description"] = textSplit[5].replace("\t\t\t", "");
+        return dataObj;
+      }
+      return [];
+    });
   });
   console.log("Resultados abaixo:");
   console.log(headings);
